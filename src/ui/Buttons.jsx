514a24a6a868871fd1f75a97cbd23acec2c170cc
demessietry <demessiewondimu12@gmail.com>
2024-04-02 13:13:48 +0300

    add env

--- src/ui/Buttons.jsx
+++ src/ui/Buttons.jsx
@@ -13,6 +13,12 @@ function Buttons({ children, disabled, to, type, onClick }) {
       "inline-block rounded-full bg-transparent border-2 border-stone-300 text-sm font-semibold uppercase tracking-wider text-stone-400 hover:bg-stone-300 hover:text-stone-800 focus:outline-none focus:ring focus:ring-stone-300 focus:text-stone-800 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5 ",
   };
 
+  if (to && onClick)
+    return (
+      <Link onClick={onClick} className={style[type]} to={to}>
+        {children}
+      </Link>
+    );
   if (to)
     return (
       <Link className={style[type]} to={to}>
